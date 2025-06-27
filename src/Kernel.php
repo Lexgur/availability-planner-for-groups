d8e418fb51352593d0ff285dbe517cb22cd953b1
edgaras <edgaras.malukas@gmail.com>
2025-06-27 18:54:09 -0400

    - Kernel.php code ignored

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,8 +5,6 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-/** @phpstan-ignore-next-line  */
-#[CodeCoverageIgnore]
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
