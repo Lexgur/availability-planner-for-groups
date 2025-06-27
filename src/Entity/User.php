d921f1a28b4fcc4d5488a5cf1df61d2e164ce86e
edgaras <edgaras.malukas@gmail.com>
2025-06-27 16:36:16 -0400

    Registration form added, symfony/mailer package installed.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,11 +14,13 @@ use App\Entity\Traits\UuidTrait;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use PHPUnit\Framework\Attributes\CodeCoverageIgnore;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\HasLifecycleCallbacks]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL_HASH', fields: ['emailHash'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements PasswordAuthenticatedUserInterface
 {
     use EmailTrait;
