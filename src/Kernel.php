66f9566315fe727d1e00b1603ddcc5ace292b187
edgaras <edgaras.malukas@gmail.com>
2025-06-27 19:45:02 -0400

    - testing.

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,6 +7,8 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+/** @phpstan-ignore-next-line */
+#[CodeCoverageIgnore]
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
