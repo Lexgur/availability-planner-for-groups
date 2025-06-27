87455fab8b1bde578895f3cb0e1631f7324102bd
edgaras <edgaras.malukas@gmail.com>
2025-06-27 17:43:58 -0400

    - Kernel, AppFixtures<dir> and Story<dir> have been CodeCoverage ignored.

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,8 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+/** @phpstan-ignore-next-line  */
+#[CodeCoverageIgnore]
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
