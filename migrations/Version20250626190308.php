802d8196bd04260eb9cb169cf8fed0ab97960ca4
edgaras <edgaras.malukas@gmail.com>
2025-06-27 18:38:35 -0400

    - comments removed from the migration so that it would work both on MariaDB and SQlite

--- migrations/Version20250626190308.php
+++ migrations/Version20250626190308.php
@@ -21,7 +21,7 @@ final class Version20250626190308 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE user (uuid VARCHAR(36) NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, email_hash VARCHAR(64) NOT NULL, roles JSON NOT NULL COMMENT '(DC2Type:json)', created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', updated_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL_HASH (email_hash), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE user (uuid VARCHAR(36) NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, email_hash VARCHAR(64) NOT NULL, roles JSON NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL_HASH (email_hash), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
     }
 
