f894cff53ac75ec60257f9bbdbe991265b4952fc
edgaras <edgaras.malukas@gmail.com>
2025-06-27 19:50:26 -0400

    -No longer attempting to exclude Kernel.php from code coverage.

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,8 +7,6 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-/** @phpstan-ignore-next-line */
-#[CodeCoverageIgnore]
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
