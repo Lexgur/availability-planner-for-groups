251afed0091483c0f312ab463d863f88dc67ec3d
edgaras <edgaras.malukas@gmail.com>
2025-06-27 20:13:29 -0400

    Needed composer packages installed.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
