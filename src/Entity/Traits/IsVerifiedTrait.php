22f7644bd970329cba91aa4c466b20f6cddaaece
edgaras <edgaras.malukas@gmail.com>
2025-06-27 20:24:36 -0400

    Added: - User entity - User repository - Tests

---
+++ src/Entity/Traits/IsVerifiedTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait IsVerifiedTrait
+{
+    #[ORM\Column(type: 'boolean')]
+    private bool $isVerified = false;
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+}
