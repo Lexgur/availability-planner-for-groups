2e80d80d868d97849236f8e74d70e85b9c91d2ae
edgaras <edgaras.malukas@gmail.com>
2025-06-28 17:21:01 -0400

    - Columns UpdatedAtTrait.php and CreatedAtTrait.php seperated into separated traits as it should have been.

--- src/Entity/Traits/UpdatedAtTrait.php
+++ src/Entity/Traits/UpdatedAtTrait.php
@@ -1,23 +1,12 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Entity\Traits;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
-trait TimestampableTrait
+trait UpdatedAtTrait
 {
-    #[ORM\Column(
-        name: 'created_at',
-        type: 'datetime_immutable',
-        nullable: true,
-        updatable: false
-    )]
-    #[Gedmo\Timestampable(on: 'create')]
-    private ?\DateTimeImmutable $createdAt = null;
-
     #[ORM\Column(
         name: 'updated_at',
         type: 'datetime_immutable',
@@ -26,15 +15,6 @@ trait TimestampableTrait
     #[Gedmo\Timestampable(on: 'update')]
     private ?\DateTimeImmutable $updatedAt = null;
 
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $createdAt): void
-    {
-        $this->createdAt = $createdAt;
-    }
 
     public function getUpdatedAt(): ?\DateTimeImmutable
     {
